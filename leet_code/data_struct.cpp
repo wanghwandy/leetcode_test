08f9ccf88e5072f3754e25541ac4534ec9fe4b08
wanghwandy <wanghw.lei@gmail.com>
2015-02-09 22:33:03 +0800

    ss

---
+++ leet_code/data_struct.cpp
@@ -0,0 +1,18 @@
+#include "data_struct.h"
+/*构造单链表*/
+void struct_soulation::create_link_list(struct ListNode **l_node,vector<int> &v_data){
+	struct ListNode *p_node,*q_node;
+	*l_node = (struct ListNode *)malloc(sizeof(struct ListNode));
+	if (*l_node != NULL){
+		p_node = (*l_node);
+		for (int i = 0; i < v_data.size(); i++){
+			q_node = (struct ListNode *)malloc(sizeof(struct ListNode));
+			if (q_node != NULL){
+				p_node->next = q_node;
+				q_node->val = v_data.at(i);
+				p_node = q_node;
+			}
+		}
+		p_node->next = NULL;
+	}
+}
---
+++ leet_code/data_struct.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct ListNode{
+	int val;
+	struct ListNode *next;
+};
+class struct_soulation
+{
+public:
+	/*create signly link list*/
+	void create_link_list(struct ListNode **l_node,vector<int> &v_data);
+	struct_soulation();
+	~struct_soulation();
+
+private:
+
+};
+
